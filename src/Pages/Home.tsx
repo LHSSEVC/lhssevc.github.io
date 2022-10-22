b11be91fc27dd5550f5a107848fc3cd9465edb3f
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-10-22 00:12:05 -0400

    Color change

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Laurel Heights Secondary School EVC Club!</h1>
+      <h1>Laurel Heights Secondary School EVC Club</h1>
       <img src='../../1.jpg' alt="img1.jpg"></img>
     </div>
   )
