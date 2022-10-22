01ba4d70e3d274d8c2bfc9d9d883df0847e2361a
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-10-21 23:37:25 -0400

    Setup Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
