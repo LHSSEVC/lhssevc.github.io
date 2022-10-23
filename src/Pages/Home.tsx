e90d334fce3400ed08bc02aa5f0e1b7e0d0ac661
PizzaPrix <seanwaterloo2997@gmail.com>
2022-10-22 22:17:13 -0400

    Add text animations

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import "../Styles/Home.css"
+import '../Styles/Home.css'
 
 const Home = () => {
   return (
     <div>
-      <h1>Laurel Heights Secondary School EVC Club</h1>
-      <img src='../../1.jpg' alt="img1.jpg"></img>
+      <img src='../../1.jpg' alt='img1.jpg'></img>
+      <div className='title'>
+        <h1>Laurel Heights Secondary School EVC Club</h1>
+      </div>
       <br/>
-      <div className="content">
+      <div className='content'>
         <h2>Welcome!</h2>
         <h3>Meetings on Wednesdays, after school.</h3>
         <p>
