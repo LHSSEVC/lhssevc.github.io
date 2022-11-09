d9d43b731a73b605096a5d8646e1014973044a78
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-11-09 14:59:32 -0500

    some stuff lol

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,19 +3,9 @@ import '../Styles/Home.scss'
 
 const Home = () => {
   return (
-    <div>
-      <img src='../../1.jpg' alt='img1.jpg'></img>
-      <div className='title'>
-        <h1>LHSS Electric Vehicle Club</h1>
-      </div>
-      <br/>
-      <div className='content'>
-        <h2>Welcome!</h2>
-        <h3>Meetings on Wednesdays, after school.</h3>
-        <p>
-        LHSS's Electric Vehicle Club is working on producing a new vehicle to compete in the University of Waterloo's Electric Vehicle Challenge.
-        </p>
-      </div>
+    <div className="Home">
+      {/* <img className="background" src="../../1.jpg"/> */}
+      <h2>Laurel Heights<br/>Electric Vehicle<br/>Club</h2>
     </div>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,15 +18,22 @@ const Navbar = () => {
     <div className="navbar">
       <div className="topbar">
         <div className="desktop links">
+          <img src="../../logo192.png" alt="Laurel Heights Secondary School Electric Vehicle Club Logo" onClick = {
+            () => {
+              window.location.href = '/'
+            }
+          }/>
           {pages.map((value, index) => {
             if (typeof value === "object") {
-              return <a href={`/`}>{value.name}</a>;
+              return <a href={`/`} key={value.name}>{value.name}</a>;
             }
             return (
               <a href={`/${value.toLowerCase().replace(" ", "")}`}>{value}</a>
             );
           })}
         </div>
+        
+        <img className="mobile" src="../../logo192.png" alt="Laurel Heights Secondary School Electric Vehicle Club Logo" />
 
         <div className="mobile hamburgerButton">
           <button
