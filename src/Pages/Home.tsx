0a723253062dc326c0fb8ffb4d4c66b9e8a70839
Daniel Zhang <d.zhang200788@gmail.com>
2022-11-16 15:37:42 -0500

    redid intro page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import '../Styles/Home.scss'
+import React from "react";
+import "../Styles/Home.scss";
 
 const Home = () => {
   return (
     <div className="Home">
-      {/* <img className="background" src="../../1.jpg"/> */}
-      <h2>Laurel Heights<br/>Electric Vehicle<br/>Club</h2>
+      <div className="landing">
+        <img className="background" alt="image of a car" src="../../1.jpg" />
+        <h2>
+          Laurel Heights
+          <br />
+          Electric Vehicle
+          <br />
+          Club
+        </h2>
+      </div>
+      <div className="accent-block">
+        <h3>Welcome</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
