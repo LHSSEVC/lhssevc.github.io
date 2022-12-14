b834e45ee4f1220be750b1619aa71a429edb20e6
Daniel Zhang <d.zhang200788@gmail.com>
2022-12-14 15:50:08 -0500

    added funny graphic

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography, Box } from "@mui/material";
+import { ReactComponent as Graphic0 } from "../assets/graphic0.svg";
 import "../Styles/Home.scss";
 
 const Home = () => {
@@ -15,18 +16,22 @@ const Home = () => {
           Club
         </Typography>
       </Box>
-      <Box className="accent-block">
+      <Box className="accent block">
         <Typography variant="h2">Welcome</Typography>
         <Typography variant="body1" sx={{ color: "background.default" }}>
           Have an interest, or willingness to learn about design, circuitry, or
           manufacturing?
           <br />
-          Ready to work hard, race vehicles, and bring glory and valour to our
-          school?
+          Ready to work hard, make your mom proud, race vehicles, and bring
+          glory and valour to our school?
           <br />
-          Join us at lunch on Wednesdays, in room{" "}
+          Join us at lunch on Wednesdays, in room 1106
           {/*TODO add room number (I forget it)*/}
         </Typography>
+      </Box>
+
+      <Box>
+        <Graphic0 height="50%" width="50%" viewBox="0 0 2100 2100" />
       </Box>
     </div>
   );
